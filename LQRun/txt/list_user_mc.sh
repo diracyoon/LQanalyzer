d548e354c7719c4c9be594c6444d504b8a918352
Inseok Yoon <iyoon@cern.ch>
2017-03-28 14:50:01 +0900

    fixing codes for v8-0-6 update

--- LQRun/txt/list_user_mc.sh
+++ LQRun/txt/list_user_mc.sh
@@ -14,4 +14,4 @@ declare -a hn=('DYJets_10to50'  'DYJets' 'WW' 'ZZ' 'WZ' 'TTJets_MG')
 
 declare -a pu_dilepton_list=('DYJets_10to50' 'DYJets' 'WJets' 'TT_powheg'  'SingleTop_s' 'SingleTbar_t' 'SingleTop_t'  'SingleTbar_tW' 'SingleTop_tW' 'WGtoLNuG'  'ZGto2LG' 'WZTo3LNu_powheg' 'ZZTo4L_powheg' 'DYJets_MG_10to50' 'DYJets_MG' 'TTJets_aMC' )
 
-declare -a ch_cb=('SKTT_powheg')
+declare -a ch_cb=('TT_powheg' 'CHToCB_M125_madgraph_13TeV_2016')
